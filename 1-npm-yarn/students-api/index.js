b2877e0295b14b74282db4222f279508c82566e6
Erick Jimenez <erick-rubi@hotmail.com>
2024-01-25 19:43:28 -0600

    levantamiento de servidor

--- 1-npm-yarn/students-api/index.js
+++ 1-npm-yarn/students-api/index.js
@@ -1,4 +1,23 @@
 // codigo para levantar nuestra API en expressJS
 
 // 1. Crear un endpoint GET en express que devuelta una lista estudiantes.
+const express = require ('express');
+const app = express();
+const port = 3000;
+
 // 2. La lista de estudiantes puede ser un arreglo guardado en una variable
+const listaEstudiantes = [
+    { id: 1, name: 'Erick', age: 38 },
+    { id: 2, name: 'Carly', age: 40 },
+];
+
+// Endpoint GET para obtener la lista de estudiantes
+app.get('/', (req, res) => {
+    res.json(listaEstudiantes);
+});
+
+// Inicia el servidor
+
+app.listen(port, () => {
+    console.log(`Servidor arrancó en el puerto ${port}`);
+});
