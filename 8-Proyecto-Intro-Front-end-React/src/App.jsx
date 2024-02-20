fbd6761decdafbc1e54574f6c0b83f420c5874df
Erick Jimenez <erick-rubi@hotmail.com>
2024-02-20 15:02:18 -0600

    Consumiendo la API para peliculas

--- 8-Proyecto-Intro-Front-end-React/src/App.jsx
+++ 8-Proyecto-Intro-Front-end-React/src/App.jsx
@@ -1,31 +1,32 @@
-import { useEffect, useState } from 'react-router-dom'
-import routes from './routes/RoutesIndex'
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
 
-  const [movies, setMovies] = useState([])
+  const [peliculas, setpeliculas] = useState([])
 
-  const dataMovies = async () => {
-    const url = 'https://api.themoviedb.org/3/trending/all/?api_key=3ce302271da4a1000763b026bd6d6a80'
-    const res = await fetch('url')
+  const obtenerPeliculas = async () => {
+    const url = 'https://api.themoviedb.org/3/trending/all/week?api_key=3ce302271da4a1000763b026bd6d6a80'
+    const res = await fetch (url)
     const data = await res.json()
-    setMovies(data.results)
+    setpeliculas(data.results)
+    console.log(data);
+
   }
 
   useEffect(() => {
-    datasMovies()
+    obtenerPeliculas()
   }, [])
 
   return (
     <div>
-
       {
-        movies.map(movie => (
-          <div key={movie.id}>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}></img>
-            <p> {movie.title} </p>
+        peliculas.map( pelicula => (
+          <div key={pelicula.id}>
+            <img src={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`} alt={pelicula.title}></img>
+            <p >{pelicula.title}</p>
           </div>
+          
         ))
       }
     </div>
